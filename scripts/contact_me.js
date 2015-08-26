92d1dbd3f4bfe13efffe2f37d84f67345ae02734
elklein96 <elklein96@gmail.com>
2015-08-26 17:48:11 -0400

    Submitting form

---
+++ scripts/contact_me.js
@@ -0,0 +1,25 @@
+$(function() {
+
+    $("input,textarea").jqBootstrapValidation({
+        preventSubmit: true,
+        submitError: function($form, event, errors) {
+        },
+        submitSuccess: function($form, event) {
+            event.preventDefault(); 
+            var firstName = $("input#Form1").val();
+        },
+        filter: function() {
+            return $(this).is(":visible");
+        },
+    });
+
+    $("a[data-toggle=\"tab\"]").click(function(e) {
+        e.preventDefault();
+        $(this).tab("show");
+    });
+});
+
+
+$('#name').focus(function() {
+    $('#success').html('');
+});
