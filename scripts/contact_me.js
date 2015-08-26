5b56d3c07f64f4477d4e5cab41b08b40ce27e5ca
elklein96 <elklein96@gmail.com>
2015-08-26 18:26:17 -0400

    Submitting form

--- scripts/contact_me.js
+++ scripts/contact_me.js
@@ -21,7 +21,7 @@ $(function() {
 
                 },
                 success: function(data) {
-                    $('#success').append('<p class="lead">Thanks for registering, '+$('#Field1').val()+'!</p>')
+                    $('#success').append('<p class="lead">Thanks for registering, '+$('#Field1').val()+'!</p>').hide().fadeIn(750));;
                 }
             });
         },
