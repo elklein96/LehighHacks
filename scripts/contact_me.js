beed4797bd4b1745e62c7ea00da31c14f4ee0257
elklein96 <elklein96@gmail.com>
2015-08-26 18:04:41 -0400

    Submitting form

--- scripts/contact_me.js
+++ scripts/contact_me.js
@@ -5,8 +5,25 @@ $(function() {
         submitError: function($form, event, errors) {
         },
         submitSuccess: function($form, event) {
-            event.preventDefault(); 
-            var firstName = $("input#Form1").val();
+            var firstHackathon;
+            $("#Field6").is(':checked') ? firstHackathon = true : firstHackathon = false;
+
+            $.ajax({
+                url:    "./scripts/register.php",
+                type:   "POST",
+                data:   {
+                    firstName:  $('#Field1').val(),
+                    lastName:  $('#Field2').val(),
+                    email:  $('#Field3').val(),
+                    school:  $('#Field4').val(),
+                    resume: $('#Field5').val(),
+                    first: firstHackathon
+
+                },
+                success: function(data) {
+                    $('#success').append('<p class="lead">Thanks for registering, '+$('#Field1').val()+'!</p>')
+                }
+            });
         },
         filter: function() {
             return $(this).is(":visible");
