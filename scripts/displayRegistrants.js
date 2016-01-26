5f73e9e07371317a5762a4ecd5e73b4b4667a555
elklein96 <elklein96@gmail.com>
2016-01-25 21:34:40 -0500

    admin display

--- scripts/displayRegistrants.js
+++ scripts/displayRegistrants.js
@@ -19,7 +19,6 @@ $(document).ready(function () {
         json = $.parseJSON(data);
         for(var i=0; i<json.length; i++){
           console.log(json[i]);
-          //$("#record-table").append('<div class="registrant" id="registrant'+i+'"><div class="data" id="data'+i+'"><h3 class="section-heading">'+json[i].firstName+' '+json[i].lastName+'</h3></div></div>');
           $("#record-table").append('<tr class="record" id="record'+i+'" data-toggle="modal" data-target="#adminModal">'+'<th class="center">'+json[i].firstName+'</th><th class="center">'+json[i].lastName+'</th><th class="center">'+json[i].email+'</th><th class="center">'+json[i].phone+'</th></tr>');
         }
       }
@@ -27,13 +26,13 @@ $(document).ready(function () {
   });
 });
 
-/*$(document).on('click', '.record', function () {
+$(document).on('click', '.record', function () {
   currentRecord = parseInt($(this).attr('id').replace('movie', ''));
 
-  $("#firstname-label").val(json[currentMovie].firstName);
-  $("#lastname-label").val(json[currentMovie].lastName);
-  $("#email-label").val(json[currentMovie].email);
-  $("#phone-label").val(json[currentMovie].phone);
-  $("#suggestions-label").val(json[currentMovie].suggestions);
-  $("#diet-label").val(json[currentMovie].diet);
-});*/
+  $("#firstname-label").val(json[currentRecord].firstName);
+  $("#lastname-label").val(json[currentRecord].lastName);
+  $("#email-label").val(json[currentRecord].email);
+  $("#phone-label").val(json[currentRecord].phone);
+  $("#suggestions-label").val(json[currentRecord].suggestions);
+  $("#diet-label").val(json[currentRecord].diet);
+});
